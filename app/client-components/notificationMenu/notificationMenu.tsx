9c9a1505c31e778fb2700a37a2c695f1ff095e5e
Narayana Lvsaln <88507292+LvsalnNarayana@users.noreply.github.com>
2023-10-01 10:10:37 +0000

    notificationMenu issue fix

--- app/client-components/notificationMenu/notificationMenu.tsx
+++ app/client-components/notificationMenu/notificationMenu.tsx
@@ -1,64 +1,78 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-'use client';
-import React, { useState } from 'react'
-import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
-import Menu from '@mui/material/Menu';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+"use client";
+import React, { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import Menu from "@mui/material/Menu";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 const NotificationMenu = () => {
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event: any) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    return (
-        <div>
-            <IconButton
-                id="basic-button"
-                aria-controls={open ? 'basic-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-                onClick={handleClick}
-            >
-                <Badge badgeContent={4} color="secondary">
-                    <NotificationsIcon className='!text-white' />
-                </Badge>
-            </IconButton>
-            <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                disablePortal
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                MenuListProps={{
-                    'aria-labelledby': 'basic-button',
-                }}
-                PaperProps={{
-                    style: {
-                        width: '400px',
-                    },
-                }}
-            >
-                <p className="py-2 px-4 hover:bg-gray-100 cursor-pointer" onClick={handleClose}>Profile</p>
-                <p className="py-2 px-4 hover:bg-gray-100 cursor-pointer" onClick={handleClose}>My account</p>
-                <p className="py-2 px-4 hover:bg-gray-100 cursor-pointer" onClick={handleClose}>Logout</p>
+  return (
+    <div>
+      <IconButton
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <Badge badgeContent={4} color="secondary">
+          <NotificationsIcon className="!text-white" />
+        </Badge>
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        disablePortal
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+        PaperProps={{
+          style: {
+            width: "400px",
+          },
+        }}
+      >
+        <p
+          className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
+          onClick={handleClose}
+        >
+          Profile
+        </p>
+        <p
+          className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
+          onClick={handleClose}
+        >
+          My account
+        </p>
+        <p
+          className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
+          onClick={handleClose}
+        >
+          Logout
+        </p>
+      </Menu>
+    </div>
+  );
+};
 
-            </Menu>
-        </div>
-    );
-}
-
-export default NotificationMenu
+export default NotificationMenu;
--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import UserMenu from '@/app/client-components/userMenu/userMenu';
-import NotificationMenu from '@/app/client-components/NotificationMenu/NotificationMenu';
-import Badge from '@mui/material/Badge';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import React from "react";
+import UserMenu from "../../client-components/userMenu/userMenu";
+import NotificationMenu from "../../client-components/notificationMenu/notificationMenu";
+import Badge from "@mui/material/Badge";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 const Header = () => {
-    return (
-        <header className="w-full bg-primary py-2">
-            <div className="flex justify-between items-center container max-w-screen-xl mx-auto">
-                <h1 className="text-2xl text-white font-semibold">Eventer</h1>
-                <div className="flex justify-center items-center">
-                    <div className='mr-4'>
-                        <NotificationMenu />
-                    </div>
-                    <UserMenu />
-                </div>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className="w-full bg-primary py-2">
+      <div className="flex justify-between items-center container max-w-screen-xl mx-auto">
+        <h1 className="text-2xl text-white font-semibold">Eventer</h1>
+        <div className="flex justify-center items-center">
+          <div className="mr-4">
+            <NotificationMenu />
+          </div>
+          <UserMenu />
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
